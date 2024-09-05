0dd63b324eb4043372ef18adb99a2b1f47911962
Yan Zhe Chan <yachan@u90z03s05.42kl.edu.my>
2024-09-05 22:13:21 +0800

    fix: parsing and operations
    coded the process to error handle and initiate hence parse to stack A
    added functions to manipulate linked lists
    will have to revise and probably modify operations

---
+++ includes/push_swap.h
@@ -0,0 +1,58 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/libft.h"
+# include <stdlib.h>
+# include <stddef.h>
+# include <limits.h>
+
+# define TRUE 1
+# define FALSE 0
+# define A_TO_B 0
+# define B_TO_A 1
+
+typedef struct s_node
+{
+	int				num;
+	int				push_cost;
+	int				above_median;
+	int				cheapest;
+	struct s_node	*next;
+}	node;
+
+typedef struct s_stack
+{
+	node	*top;
+	int		size;
+}	stack;
+
+/*============*/
+/*MAIN PROCESS*/
+/*============*/
+
+/*initialising and parsing stack A*/
+
+void	init_stack_a(int ac, char **av, stack **stack_a);
+void	parse_stack_a(int ac, char **av, stack **stack_a);
+
+/*parsing check*/
+
+int		parse_check(char *num_str, stack *stack);
+void	free_parse_data(stack **stack_a, char ***num_str_arr, char **num_str);
+
+/*extra utils*/
+
+void	err_and_exit(void);
+char	*combine_str(int ac, char **av);
+
+/*==========*/
+/*OPERATIONS*/
+/*==========*/
+
+/*linked list manipulations*/
+
+node	*append_node(int new_num);
+void	add_to_top(stack *stack, node *new_node);
+void	add_to_bottom(stack *stack, node *new_node);
+void	free_whole_stack(stack **stack);
+#endif
---
+++ operations/list-manipulation.c
@@ -0,0 +1,73 @@
+#include "../includes/push_swap.h"
+
+/*
+*malloc and create a new node
+*/
+node	*append_node(int new_num)
+{
+	node	*new_node;
+
+	new_node = (node *)malloc(sizeof(node));
+	if (!new_node)
+		return (NULL);
+	new_node->num = new_num;
+    new_node->push_cost = 0;
+    new_node->above_median = FALSE;
+    new_node->cheapest = FALSE;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/*
+*adds a new node to the start of a linked list
+*/
+void	add_to_top(stack *stack, node *new_node)
+{
+	if (!stack || !new_node)
+		return ;
+	new_node->next = stack->top;
+	stack->top = new_node;
+}
+
+/*
+*adds a new node to the end of a linked list
+*/
+void 	add_to_bottom(stack *stack, node *new_node)
+{
+	node	*current;
+
+	if (!stack || !new_node)
+		return;
+	if (!stack->top)
+	{
+		stack->top = new_node;
+		stack->size = 1;
+		return;
+	}
+	current = stack->top;
+	while (current->next)
+		current = current->next;
+	current->next = new_node;
+	stack->size++;
+}
+
+/*
+*frees the entire stack
+*/
+void	free_whole_stack(stack **stack)
+{
+	node	*current;
+	node	*temp;
+
+	if (!stack || !(*stack))
+		return ;
+	current = (*stack)->top;
+	while (current)
+	{
+		temp = current->next;
+		free(current);
+		current = temp;
+	}
+	(*stack)->top = NULL;
+	(*stack)->size = 0;
+}
